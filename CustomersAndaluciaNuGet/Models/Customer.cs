242453e990154989e0671f400cb56dfaeeee7237
nachocerrato <nacho.cr.94@gmail.com>
2022-01-18 17:04:11 +0100

    Add project files.

---
+++ CustomersAndaluciaNuGet/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersAndaluciaNuGet.Models
+{
+    public class Customer
+    {
+        [JsonProperty("CustomerID")]
+        public String IdCustomer { get; set; }
+        [JsonProperty("CompanyName")]
+        public String Company { get; set; }
+        [JsonProperty("ContactName")]
+        public String Name { get; set; }
+        [JsonProperty("City")]
+        public String City { get; set; }
+    }
+}
---
+++ CustomersAndaluciaNuGet/NorthWindData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using CustomersAndaluciaNuGet.Models;
+
+namespace CustomersAndaluciaNuGet
+{
+    public class NorthWindData
+    {
+        //CUANDO LLAMAMOS A UN SERVICIO LAS LLAMADAS DEBEN
+        //SER ASINCRONAS
+        public async Task<CustomerList> GetCustomerListAsync()
+        {
+            WebClient client = new WebClient();
+            client.Headers["content-type"] = "application/json";
+            string url = "https://services.odata.org/V4/Northwind/Northwind.svc/Customers";
+            string datajson = await client.DownloadStringTaskAsync(url);
+            //EXTRAEMOS LOS DATOS DE STRING Y LOS CONVERTIMOS 
+            //A CLASES UTILIZANDO NEWTONSOFT
+            CustomerList customers =
+                JsonConvert.DeserializeObject<CustomerList>(datajson);
+            return customers;
+        }
+    }
+}
